8448eab5346da7730384f25eadb0911134507786
aissam99 <aissami.ismail@gmail.com>
2018-06-11 15:02:11 +0200

    Update run.java

--- dm/src/dm/run.java
+++ dm/src/dm/run.java
@@ -5,36 +5,43 @@ public class run {
 	public static void main(String[] args)
 	  {
 		
-	    Domino dm =  new Domino();
-	    Player p1 =new Player();
-	    Player p2 =new Player();
+		Partie partie=new Partie();
+        Dominos dm =  new Dominos();
+        
+    Player p1 =new Player();
+    Player p2 =new Player();
 
-	    
-	    System.out.println("Bounjour, tes dominos que t as crée sont :");
+    
+    /*System.out.println("Bounjour, tes dominos que t as crée sont :");
+        dm.int_pioche();
+    dm.affiche_pioche();*/
+    
+    /*System.out.println("je vais essayé de mélanger les pieces :) et les afficher ");
+    dm.mix_pioche();
+    dm.affiche_pioche();*/
 
-	    dm.int_pioche();
-	    dm.affiche_pioche();
-	    
-	    System.out.println("je vais essayé de mélanger les pieces :) et les afficher ");
-	    dm.mix_pioche();
-	    dm.affiche_pioche();
+    //System.out.println("apres j'essai de distribué les cartes sur les joueurs (2 joueur dans ce cas)");
+    int[][] P= new int [28][2];
+    
+    P=p1.init_piocheP(dm.getPioche());
+    P=p2.init_piocheP(dm.getPioche());
+    
+    System.out.println("Joueur 1 :	-----------");
+    p1.affiche_piocheP();
+    
+    System.out.println("Joueur 2 :	-----------");
+    p2.affiche_piocheP();
+    
+    //System.out.println("Piéce apres distrubition !!! verifier	-----------");
+    //dm.affiche_tt_pioche(P);
+        
+        //dm.premier_coup(p1.getPiocheP(), p2.getPiocheP());
+        //System.out.println(dm.premier_joueur (p1.getPiocheP(),p2.getPiocheP())+" va Jouer en premier");
 
-	    System.out.println("apres j'essai de distribué les cartes sur les joueurs (2 joueur dans ce cas)");
-	    int[][] P= new int [28][2];
-	    
-	    P=p1.init_piocheP(dm.getPioche());
-	    P=p2.init_piocheP(dm.getPioche());
-	    
-	    System.out.println("Joueur 1 :	-----------");
-	    p1.affiche_piocheP();
-	    
-	    System.out.println("Joueur 2 :	-----------");
-	    p2.affiche_piocheP();
-	    
-	    System.out.println("Piéce apres distrubition !!! verifier	-----------");
-	    dm.affiche_tt_pioche(P);
-	    
-	    System.out.println("si tous passe bien félicitation !!");
-
+        
+        partie.setJ1(p1.getPiocheP());
+        partie.setJ2(p2.getPiocheP());
+        
+    System.out.println("si tous passe bien félicitation !!");
 	  }
 }
