3396cf8c5aa67980c98ce9d5bb0ec3ccb13db5f4
aissam99 <aissami.ismail@gmail.com>
2018-06-11 15:03:13 +0200

    Update Player.java

--- dm/src/dm/Player.java
+++ dm/src/dm/Player.java
@@ -2,37 +2,82 @@ package dm;
 
 public class Player {
 	private String pseudo;
-	private int[][] piocheP=new int[7][2];
-	private float score;
-	
-	public void Player() {
-		pseudo="Joueur";
-		score=0;
-	}
-	
-	public int[][] init_piocheP(int[][] pp){ // pp : pioches du joueur
-		int cmp=0;
-		for(int k=0;k<28;k++) {
-			if(pp[k][0]!=-1 && cmp<7) {
-				piocheP[cmp][0]=pp[k][0];
-				piocheP[cmp][1]=pp[k][1];
-				cmp++;
-				pp[k][0]=-1;
-				pp[k][1]=-1;
-			}
+private int[][] piocheP=new int[7][2];
+private float score;
 
+public void Player() {
+	setPseudo("Joueur");
+	setScore(0);
+}
+
+public int[][] init_piocheP(int[][] pp){ // pp : pioches du joueur
+	int cmp=0;
+	for(int k=0;k<28;k++) {
+		if(pp[k][0]!=-1 && cmp<7) {
+			getPiocheP()[cmp][0]=pp[k][0];
+			getPiocheP()[cmp][1]=pp[k][1];
+			cmp++;
+			pp[k][0]=-1;
+			pp[k][1]=-1;
 		}
-		return(pp);
+
 	}
-	
-	//---- Un test pour voir  tous les pioches
-		public void affiche_piocheP() {
-			int nbr=0;
-			for(int k=0;k<7;k++) {
-				nbr++;
-				System.out.println(piocheP[k][0]+" | "+piocheP[k][1]+ "		=>"+nbr);
-			}
-		}
-		
-	
+	return(pp);
+}
+
+//---- Un test pour voir  tous les pioches
+    public void affiche_piocheP() {
+        int nbr=0;
+        for(int k=0;k<7;k++) {
+            nbr++;
+            System.out.println(getPiocheP()[k][0]+" | "+getPiocheP()[k][1]+ "		=>"+nbr);
+        }
+    }
+
+            
+            
+//---- Getter et setter
+/**
+ * @return the pseudo
+ */
+public String getPseudo() {
+    return pseudo;
+}
+
+/**
+ * @param pseudo the pseudo to set
+ */
+public void setPseudo(String pseudo) {
+    this.pseudo = pseudo;
+}
+
+/**
+ * @return the piocheP
+ */
+public int[][] getPiocheP() {
+    return piocheP;
+}
+
+/**
+ * @param piocheP the piocheP to set
+ */
+public void setPiocheP(int[][] piocheP) {
+    this.piocheP = piocheP;
+}
+
+/**
+ * @return the score
+ */
+public float getScore() {
+    return score;
+}
+
+/**
+ * @param score the score to set
+ */
+public void setScore(float score) {
+    this.score = score;
+}
+            
+
 }
