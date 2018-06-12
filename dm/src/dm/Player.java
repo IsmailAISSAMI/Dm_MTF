3c4e2dd9b2f84057c76625140fbe464410523f2a
aissam99 <aissami.ismail@gmail.com>
2018-06-12 20:02:28 +0200

    Update Player.java

--- dm/src/dm/Player.java
+++ dm/src/dm/Player.java
@@ -1,83 +1,91 @@
-package dm;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package run;
 
+/**
+ *
+ * @author aissami
+ */
 public class Player {
-	private String pseudo;
-private int[][] piocheP=new int[7][2];
-private float score;
-
-public void Player() {
-	setPseudo("Joueur");
-	setScore(0);
-}
-
-public int[][] init_piocheP(int[][] pp){ // pp : pioches du joueur
-	int cmp=0;
-	for(int k=0;k<28;k++) {
-		if(pp[k][0]!=-1 && cmp<7) {
-			getPiocheP()[cmp][0]=pp[k][0];
-			getPiocheP()[cmp][1]=pp[k][1];
-			cmp++;
-			pp[k][0]=-1;
-			pp[k][1]=-1;
-		}
-
+    	private String pseudo;
+	private int[][] piocheP=new int[7][3];
+	private float score;
+	
+	public void Player() {
+		setPseudo("Joueur");
+		setScore(0);
 	}
-	return(pp);
-}
+	
+	public int[][] init_piocheP(int[][] pp){ // pp : pioches du joueur
+		int cmp=0;
+		for(int k=0;k<28;k++) {
+			if(cmp<7) {
+				getPiocheP()[cmp][0]=pp[k][0];
+				getPiocheP()[cmp][1]=pp[k][1];
+                                getPiocheP()[cmp][2]=pp[k][2];
+				cmp++;
+			}
 
-//---- Un test pour voir  tous les pioches
-    public void affiche_piocheP() {
-        int nbr=0;
-        for(int k=0;k<7;k++) {
-            nbr++;
-            System.out.println(getPiocheP()[k][0]+" | "+getPiocheP()[k][1]+ "		=>"+nbr);
+		}
+		return(pp);
+	}
+	
+	//---- Un test pour voir  tous les pioches
+        public void affiche_piocheP() {
+            int nbr=0;
+            for(int k=0;k<7;k++) {
+                nbr++;
+                System.out.println(getPiocheP()[k][0]+" | "+getPiocheP()[k][1]+ "etat=>"+getPiocheP()[k][2]);
+            }
         }
+
+                
+                
+    //---- Getter et setter
+    /**
+     * @return the pseudo
+     */
+    public String getPseudo() {
+        return pseudo;
     }
 
-            
-            
-//---- Getter et setter
-/**
- * @return the pseudo
- */
-public String getPseudo() {
-    return pseudo;
-}
+    /**
+     * @param pseudo the pseudo to set
+     */
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
 
-/**
- * @param pseudo the pseudo to set
- */
-public void setPseudo(String pseudo) {
-    this.pseudo = pseudo;
-}
+    /**
+     * @return the piocheP
+     */
+    public int[][] getPiocheP() {
+        return piocheP;
+    }
 
-/**
- * @return the piocheP
- */
-public int[][] getPiocheP() {
-    return piocheP;
-}
+    /**
+     * @param piocheP the piocheP to set
+     */
+    public void setPiocheP(int[][] piocheP) {
+        this.piocheP = piocheP;
+    }
 
-/**
- * @param piocheP the piocheP to set
- */
-public void setPiocheP(int[][] piocheP) {
-    this.piocheP = piocheP;
-}
+    /**
+     * @return the score
+     */
+    public float getScore() {
+        return score;
+    }
 
-/**
- * @return the score
- */
-public float getScore() {
-    return score;
-}
-
-/**
- * @param score the score to set
- */
-public void setScore(float score) {
-    this.score = score;
-}
-            
-
+    /**
+     * @param score the score to set
+     */
+    public void setScore(float score) {
+        this.score = score;
+    }
+                
+        
 }
